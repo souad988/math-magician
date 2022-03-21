8919e96c3f2c5307fe6faeb6722c0616b65fbdf5
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-21 12:20:22 +0100

    Change class component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,94 +10,16 @@ class Calculator extends React.Component {
       next: null,
       operation: null,
     };
-    // this.handlClick=this.handlClick.bind(this);
-  }
-
-  // operators=[{
-  //   value: '+',
-  //   symbol: '+',
-  //   id: 'Plus',
-  // },
-  // {
-  //   value: '-',
-  //   symbol: '-',
-  //   id: 'Minus',
-  // },
-  // {
-  //   value: '',
-  //   symbol: '+/-',
-  //   id: 'Inverter',
-  // },
-  // {
-  //   value: '*',
-  //   symbol: 'x',
-  //   id: 'Times',
-  // },
-  // {
-  //   value: '/',
-  //   symbol: '÷',
-  //   id: 'Divide',
-  // }];
-
-  // buttons = [
-  //   { value: 'AC',
-  //   symbol: 'AC',
-  //   id: 'AC',},
-  //   { value: '%',
-  //   symbol: '%',
-  //   id: '%',},
-  //   { value: '+/-',
-  //   symbol: '+/-',
-  //   id: '+/-',},
-  //   {
-  //     value: '%',
-  //     symbol: '%',
-  //     id: '%',
-  //   },
-  //   {
-  //     value: '1',
-  //     symbol: '1',
-  //     id: '',
-  //   },
-  //   {
-  //     value: '2',
-  //     symbol: '2',
-  //     id: 'Two',
-  //   },
-  //   {
-  //     value: '3',
-  //     symbol: '3',
-  //     id: 'Three',
-  //   },
-  //   {
-  //     value: '4',
-  //     symbol: '4',
-  //     id: 'Four',
-  //   },
-  //   {
-  //     value: '5',
-  //     symbol: '5',
-  //     id: 'Five',
-  //   },
-  //   {
-  //     value: '6',
-  //     symbol: '6',
-  //     id: 'Six',
-  //   },
-  //   {
-  //     value: '7',
-  //     symbol: '7',
-  //     id: 'Seven',
-  //   }];
-
-  handlClick=(e) => {
-    this.setState((state) => ({
-      ...state,
-      ...calculate(state, e.target.name),
-    }));
+    //  this.handlClick=this.handlClick.bind(this);
   }
 
   render() {
+    const handlClick = (e) => {
+      this.setState((state) => ({
+        ...state,
+        ...calculate(state, e.target.name),
+      }));
+    };
     const { total, next, operation } = this.state;
     return (
       <div className="calculator_container">
@@ -107,27 +29,27 @@ class Calculator extends React.Component {
           {next}
         </div>
         <div className="number_container">
-          <button className="AC" name="AC" onClick={(e) => this.handlClick(e)} type="button">AC</button>
-          <button className="pos_neg" name="+/-" onClick={(e) => this.handlClick(e)} type="button">+/-</button>
-          <button className="percent" name="%" onClick={(e) => this.handlClick(e)} type="button">%</button>
-          <button className="seven" name="7" onClick={(e) => this.handlClick(e)} type="button">7</button>
-          <button className="eight" name="8" onClick={(e) => this.handlClick(e)} type="button">8</button>
-          <button className="nine" name="9" onClick={(e) => this.handlClick(e)} type="button">9</button>
-          <button className="four" name="4" onClick={(e) => this.handlClick(e)} type="button">4</button>
-          <button className="five" name="5" onClick={(e) => this.handlClick(e)} type="button">5</button>
-          <button className="six" name="6" onClick={(e) => this.handlClick(e)} type="button">6</button>
-          <button className="one" name="1" onClick={(e) => this.handlClick(e)} type="button">1</button>
-          <button className="two" name="2" onClick={(e) => this.handlClick(e)} type="button">2</button>
-          <button className="three" name="3" onClick={(e) => this.handlClick(e)} type="button">3</button>
-          <button className="zero" name="0" onClick={(e) => this.handlClick(e)} type="button">0</button>
-          <button className="dot" name="." onClick={(e) => this.handlClick(e)} type="button">.</button>
+          <button className="AC" name="AC" onClick={(e) => handlClick(e)} type="button">AC</button>
+          <button className="pos_neg" name="+/-" onClick={(e) => handlClick(e)} type="button">+/-</button>
+          <button className="percent" name="%" onClick={(e) => handlClick(e)} type="button">%</button>
+          <button className="seven" name="7" onClick={(e) => handlClick(e)} type="button">7</button>
+          <button className="eight" name="8" onClick={(e) => handlClick(e)} type="button">8</button>
+          <button className="nine" name="9" onClick={(e) => handlClick(e)} type="button">9</button>
+          <button className="four" name="4" onClick={(e) => handlClick(e)} type="button">4</button>
+          <button className="five" name="5" onClick={(e) => handlClick(e)} type="button">5</button>
+          <button className="six" name="6" onClick={(e) => handlClick(e)} type="button">6</button>
+          <button className="one" name="1" onClick={(e) => handlClick(e)} type="button">1</button>
+          <button className="two" name="2" onClick={(e) => handlClick(e)} type="button">2</button>
+          <button className="three" name="3" onClick={(e) => handlClick(e)} type="button">3</button>
+          <button className="zero" name="0" onClick={(e) => handlClick(e)} type="button">0</button>
+          <button className="dot" name="." onClick={(e) => handlClick(e)} type="button">.</button>
         </div>
         <div className="operators">
-          <button className="divide" name="÷" onClick={(e) => this.handlClick(e)} type="button">÷</button>
-          <button className="multiply" name="x" onClick={(e) => this.handlClick(e)} type="button">x</button>
-          <button className="add" name="+" onClick={(e) => this.handlClick(e)} type="button">+</button>
-          <button className="substruct" name="-" onClick={(e) => this.handlClick(e)} type="button">-</button>
-          <button className="equal" name="=" onClick={(e) => this.handlClick(e)} type="button">=</button>
+          <button className="divide" name="÷" onClick={(e) => handlClick(e)} type="button">÷</button>
+          <button className="multiply" name="x" onClick={(e) => handlClick(e)} type="button">x</button>
+          <button className="add" name="+" onClick={(e) => handlClick(e)} type="button">+</button>
+          <button className="substruct" name="-" onClick={(e) => handlClick(e)} type="button">-</button>
+          <button className="equal" name="=" onClick={(e) => handlClick(e)} type="button">=</button>
         </div>
       </div>
     );
