6327c322f2ed038b6cd39a98bc39d561b5dbd107
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-23 23:37:34 +0100

    Add react routes and add links in header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +15,13 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/Calculator" element={<Calculator />} />
+            <Route path="/Quote" element={<Quote />} />
+          </Route>
+        </Routes>
       </div>
     );
   }
